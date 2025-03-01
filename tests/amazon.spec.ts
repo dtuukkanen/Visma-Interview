af201c74c19e05244f3ffe46a84aa5bc50024310
Daniel Tuukkanen <tuukkanen.daniel@gmail.com>
2025-03-01 21:43:05 +0200

    Bit better constants

--- tests/amazon.spec.ts
+++ tests/amazon.spec.ts
@@ -3,14 +3,15 @@ import { test, expect } from "@playwright/test";
 test("Visma Interview", async ({ page }) => {
   // Website URL
   const url = "https://www.amazon.com/";
+  const searchTerm = "Nikon";
   const expectedTitle = "Nikon D3X";
 
   // Open the page
   await page.goto(url);
 
   // Search for Nikon
-  await page.waitForSelector("#twotabsearchtextbox");
-  await page.fill("#twotabsearchtextbox", "Nikon");
+  const search = await page.waitForSelector("#twotabsearchtextbox");
+  await search.fill(searchTerm);
   await page.click("#nav-search-submit-button");
 
   // Wait for sorting dropdown to be available
@@ -22,18 +23,13 @@ test("Visma Interview", async ({ page }) => {
   await page.click("a#s-result-sort-select_2");
 
   // Select and click the second product in the results
-  const secondResult = page
-    .locator("[data-component-type='s-search-result']")
-    .nth(1);
+  const secondResult = page.locator("[data-component-type='s-search-result']").nth(1);
   await secondResult.scrollIntoViewIfNeeded();
-  await secondResult
-    .locator(
-      "h2 a.a-link-normal, span[data-component-type='s-product-image'] a"
-    )
-    .first()
-    .click();
+  // Find the link inside the second result
+  const secondResultLink = secondResult.locator("h2 a.a-link-normal, span[data-component-type='s-product-image'] a").first()
+  await secondResultLink.click();
 
   // Get and verify product title
-  const productTitle = await page.innerText("#productTitle");
+  const productTitle = await page.locator("#productTitle").innerText();
   expect(productTitle.trim()).toContain(expectedTitle);
 });
